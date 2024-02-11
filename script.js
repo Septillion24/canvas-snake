bfe8710b757ce272e623fe023b6d17a943e9020a
Booperboberino <ngrosh@uncc.edu>
2024-02-11 16:05:17 -0500

    added base document

---
+++ script.js
@@ -0,0 +1,14 @@
+let canvas = document.getElementById("main-canvas");
+
+function showError(errorText) {
+    const errorBoxDiv = document.getElementById("error-box");
+    const errorTextElement = document.createElement("p");
+    errorTextElement.innerText = errorText;
+    errorBoxDiv.appendChild(errorTextElement);
+    console.log(errorText);
+}
+
+function main() 
+{
+    
+}
