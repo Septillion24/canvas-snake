8a5ac79791877d2796c758063e3eea04e6e745a8
Septillion24 <noahgrosh@gmail.com>
2024-02-12 09:47:04 -0500

    added drawGridSquare

--- script.js
+++ script.js
@@ -1,4 +1,18 @@
-let canvas = document.getElementById("main-canvas");
+let canvas = document.getElementById("demo-canvas");
+let ctx = canvas.getContext("2d");
+let canvasWidth = canvas.width;
+let canvasHeight = canvas.height;
+
+const gridWidth = 10;
+const gridHeight = 10;
+var grid = [];
+grid.length = gridWidth;
+for (let index = 0; index < gridWidth; index++) {
+    grid[index] = new Array(gridHeight);
+}
+grid[2][1] = 1;
+
+
 
 function showError(errorText) {
     const errorBoxDiv = document.getElementById("error-box");
@@ -7,8 +21,32 @@ function showError(errorText) {
     errorBoxDiv.appendChild(errorTextElement);
     console.log(errorText);
 }
+function drawGridSquare(x, y, color = "black") {
 
-function main() 
-{
+    if(x > gridWidth-1)
+    {
+        throw new Error(`Index out of range. ${x} is greater than max index ${gridWidth-1}`);
+    }
+    if(y > gridHeight-1)
+    {
+        throw new Error(`Index out of range. ${y} is greater than max index ${gridHeight-1}`);
+    }
     
+    ctx.fillStyle = "black";
+    const gridSizeX = canvasWidth / gridWidth;
+    const xPos = gridSizeX * x;
+
+    const gridSizeY = canvasHeight / gridHeight;
+    const yPos = gridSizeY * y;
+
+    ctx.fillRect(xPos, yPos, gridSizeX, gridSizeY);
+}
+
+
+
+
+try {
+    drawGridSquare(1,9)
+} catch (e) {
+    showError(e);
 }
