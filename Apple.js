71987582fb91808070838a04dcb3626419fc0111
Septillion24 <noahgrosh@gmail.com>
2024-02-12 10:32:01 -0500

    added apple

---
+++ Apple.js
@@ -0,0 +1,7 @@
+class Apple{
+    constructor(x,y)
+    {
+        this.x = x;
+        this.y = y;
+    }
+}
--- script.js
+++ script.js
@@ -2,27 +2,10 @@ let canvas = document.getElementById("demo-canvas");
 let ctx = canvas.getContext("2d");
 let canvasWidth = canvas.width;
 let canvasHeight = canvas.height;
-
 const gridWidth = 10;
 const gridHeight = 10;
-var grid = [];
-grid.length = gridWidth;
-for (let index = 0; index < gridWidth; index++) {
-    grid[index] = new Array(gridHeight);
-}
-grid[2][1] = 1;
-const head = new SnakePart(Math.floor(gridWidth/2),Math.floor(gridHeight/2));
-head.next = new SnakePart(Math.floor(gridWidth/2)+1,Math.floor(gridHeight/2));
-
-
-for (let current = head; head.next != null; current = current.next) {
-    drawGridSquare(current.x,current.y, color="green");
-}
-
-
-
-showError("e");
-
+let head;
+let apples = [];
 
 function showError(errorText) {
     const errorBoxDiv = document.getElementById("error-box");
@@ -32,16 +15,13 @@ function showError(errorText) {
     console.log(errorText);
 }
 function drawGridSquare(x, y, color = "black") {
+    if (x > gridWidth - 1) {
+        throw new Error(`Index out of range. ${x} is greater than max index ${gridWidth - 1}`);
+    }
+    if (y > gridHeight - 1) {
+        throw new Error(`Index out of range. ${y} is greater than max index ${gridHeight - 1}`);
+    }
 
-    if(x > gridWidth-1)
-    {
-        throw new Error(`Index out of range. ${x} is greater than max index ${gridWidth-1}`);
-    }
-    if(y > gridHeight-1)
-    {
-        throw new Error(`Index out of range. ${y} is greater than max index ${gridHeight-1}`);
-    }
-    
     ctx.fillStyle = color;
     const gridSizeX = canvasWidth / gridWidth;
     const xPos = gridSizeX * x;
@@ -51,12 +31,28 @@ function drawGridSquare(x, y, color = "black") {
 
     ctx.fillRect(xPos, yPos, gridSizeX, gridSizeY);
 }
-
-
+function createNewSnake() {
+    head = new SnakePart(Math.floor(gridWidth / 2), Math.floor(gridHeight / 2));
+    head.next = new SnakePart(Math.floor(gridWidth / 2) + 1, Math.floor(gridHeight / 2));
+}
+function displaySnake() {
+    for (let current = head; current != null; current = current.next) {
+        drawGridSquare(current.x, current.y, (color = "green"));
+    }
+}
+function displayAllApples(){
+    apples.forEach(apple => {
+       drawGridSquare(apple.x,apple.y, "#b30909"); 
+    });
+}
 
 
 try {
-    drawGridSquare(1,9)
+    apples.push(new Apple(2,2));
+    displayAllApples();
+    createNewSnake();
+    displaySnake();
+    // drawGridSquare(1, 1);
 } catch (e) {
     showError(e);
 }
